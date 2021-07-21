635b2c7143d615b1d60de840c14db5bcb81dcc9a
manhtq <manhtq@sunshinegroup.vn>
2021-07-21 17:26:12 +0700

    save point

--- Framework/arraymethods.php
+++ Framework/arraymethods.php
@@ -25,4 +25,19 @@ class ArrayMethods
             return trim($item);
         }, $array);
     }
+
+    public function flatten ($array, $return = array())
+    {
+        foreach ($array as $key => $value)
+        {
+            if (is_array($value) || is_object($value))
+            {
+                return = self::flatten($value, $return);
+            } else {
+                $return[] = $value;
+            }
+        }
+
+        return $return;
+    }
 }
